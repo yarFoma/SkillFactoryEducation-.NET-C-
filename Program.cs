fd50881a3e491841df0693ea16c5afd9950f57d6
yarFoma <yar.foma@yandex.ru>
2025-05-17 13:49:56 +0300

    Returnded last lesson by SkillFactory 4.1

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿var color = Console.ReadLine();
+
+switch (color)
+{
+    case "red":
+        Console.BackgroundColor = ConsoleColor.Red;
+        Console.ForegroundColor = ConsoleColor.Black;
+
+        Console.WriteLine("Your color is red!");
+        break;
+
+    case "green":
+        Console.BackgroundColor = ConsoleColor.Green;
+        Console.ForegroundColor = ConsoleColor.Black;
+
+        Console.WriteLine("Your color is green!");
+        break;
+
+    default:
+        Console.BackgroundColor = ConsoleColor.Cyan;
+        Console.ForegroundColor = ConsoleColor.Black;
+
+        Console.WriteLine("Your color is cyan!");
+        break;
+}
